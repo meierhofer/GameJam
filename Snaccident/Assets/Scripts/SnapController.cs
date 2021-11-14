2a67b4b005b737153062b2810f43e7c9d0d14780
k.meierhofer <karoline.meierhofer@gmail.com>
2021-11-14 13:06:55 +0100

    tiny changes

--- Snaccident/Assets/Scripts/SnapController.cs
+++ Snaccident/Assets/Scripts/SnapController.cs
@@ -6,7 +6,7 @@ public class SnapController : MonoBehaviour
 {
     public List<Transform> snapPoints;
     public List<Draggable> draggableObjects;
-    public float snapRange = 0.05f;
+    public float snapRange = 0.02f;
 
     // Start is called before the first frame update
     void Start()
@@ -24,7 +24,8 @@ public class SnapController : MonoBehaviour
 
         foreach(Transform snapPoint in snapPoints)
         {
-            float currentDistance = Vector2.Distance(draggable.transform.localPosition, snapPoint.localPosition);
+            float currentDistance = Vector2.Distance(draggable.transform.position, snapPoint.position);
+            currentDistance = Mathf.Abs(currentDistance);
             if(closestSnapPoint == null || currentDistance < closestDistance)
             {
                 closestSnapPoint = snapPoint;
@@ -33,7 +34,12 @@ public class SnapController : MonoBehaviour
         }
         if(closestSnapPoint != null && closestDistance <= snapRange)
         {
-            draggable.transform.localPosition = closestSnapPoint.localPosition;
+            draggable.transform.position = closestSnapPoint.position;
         }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
     }
 }
